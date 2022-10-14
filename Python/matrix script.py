2af4f1a0f4d766a6eec44b564af3d5d6c4c71ba1
Satyaki Mandal <satyakimandal19052001@gmail.com>
2022-10-14 20:44:35 +0530

    Update matrix script.py
    The question description has been removed and the Original question link has been highlighted

--- Python/matrix script.py
+++ Python/matrix script.py
@@ -1,14 +1,5 @@
-# original question: https://www.hackerrank.com/challenges/matrix-script/
-
 '''
-Neo has a complex matrix script. The matrix script is a  X  grid of strings. It consists of alphanumeric characters,
-spaces and symbols (!,@,#,$,%,&).
-
-To decode the script, Neo needs to read each column and select only the alphanumeric characters and connect them.
-Neo reads the column from top to bottom and starts reading from the leftmost column.
-If there are symbols or spaces between two alphanumeric characters of the decoded script, then Neo replaces them with a single space '' for better readability.
-Neo feels that there is no need to use 'if' conditions for decoding.
-Alphanumeric characters consist of: [A-Z, a-z, and 0-9].
+original question: https://www.hackerrank.com/challenges/matrix-script/
 '''
 
 #Explaination: Use a nested loop to concatenate the elements columnwise starting from the top left;
