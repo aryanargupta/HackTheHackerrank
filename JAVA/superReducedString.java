c36d951305254a65ea38e41309b70d64503670a7
Rishi Sahu <l.rishissahu@gmail.com>
2022-10-20 17:01:53 +0530

    Super Reduced String Solution issue #4

---
+++ JAVA/superReducedString.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superReducedString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String superReducedString(String s) {
+        // Write your code here
+        StringBuilder sb = new StringBuilder(s);
+        for(int i =0;  i < sb.length() - 1; i++){
+            if(sb.charAt(i)==sb.charAt(i+1)){
+                sb.delete(i, i+2);
+                i=-1;
+            }
+        }
+        if(sb.length()==0) return "Empty String";
+        else return String.valueOf(sb);
+
+    }
+
+}
+
+public class superReducedString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.superReducedString(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
