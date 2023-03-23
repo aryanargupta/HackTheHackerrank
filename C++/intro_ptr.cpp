5d1965ff882e83c6efc2b05b13ad899d2775d3bf
Harshit9369 <119619598+Harshit9369@users.noreply.github.com>
2023-03-23 17:12:16 +0530

    Add files via upload

---
+++ C++/intro_ptr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int x = a + b;
+    int y = a - b;
+    int z = abs(y);
+    cout << x << endl << z << endl;
+    return 0;
+}
